c5bb943a78e1651f0f7bd2731e448927ea567234
Sluym1k <Denis.kerstens@gmail.com>
2025-12-11 17:03:37 +0300

    changes to audio distortion, and fixed stack amount to one

--- src/main/java/com/sluym1k/simplemegaphone/SimpleMegaphoneVoicechatPlugin.java
+++ src/main/java/com/sluym1k/simplemegaphone/SimpleMegaphoneVoicechatPlugin.java
@@ -2,11 +2,14 @@ package com.sluym1k.simplemegaphone;
 
 import com.sluym1k.simplemegaphone.item.ModItems;
 import de.maxhenkel.voicechat.api.VoicechatApi;
+import de.maxhenkel.voicechat.api.events.ClientSoundEvent;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent;
 import de.maxhenkel.voicechat.api.events.VoiceDistanceEvent;
 import de.maxhenkel.voicechat.api.opus.OpusDecoder;
 import de.maxhenkel.voicechat.api.opus.OpusEncoder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.level.ServerPlayer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -37,7 +40,7 @@ public class SimpleMegaphoneVoicechatPlugin implements VoicechatPlugin {
     @Override
     public void registerEvents(EventRegistration registration) {
         registration.registerEvent(VoiceDistanceEvent.class, this::voiceDistance);
-        registration.registerEvent(MicrophonePacketEvent.class, this::microphonePacket);
+//        registration.registerEvent(ClientSoundEvent.class, this::clientSound);
     }
 
     public void voiceDistance(VoiceDistanceEvent e) {
@@ -50,37 +53,33 @@ public class SimpleMegaphoneVoicechatPlugin implements VoicechatPlugin {
             }
         }
     }
-    public void microphonePacket(MicrophonePacketEvent e) {
-        if (e.getSenderConnection() == null || !(e.getSenderConnection().getPlayer().getPlayer() instanceof ServerPlayer player)) {
-            return;
-        }
+    public void clientSound(ClientSoundEvent e) {
+        LocalPlayer player = Minecraft.getInstance().player;
 
         if (player.isUsingItem()) {
             if (player.getUseItem().is(ModItems.MEGAPHONE.get())) {
-                short[] audio = DECODER.decode(e.getPacket().getOpusEncodedData());
+                short[] audio = e.getRawAudio();
                 audio = distort(audio);
-                e.getPacket().setOpusEncodedData(ENCODER.encode(audio));
+                e.setRawAudio(audio);
             }
         }
     }
 
+    float lastValue = 0;
     public short[] distort(short[] audio) {
         float gain = 2.5f;
         short[] newAudio = new short[audio.length];
 
         for (int i = 0; i < audio.length; i++) {
-            float value = audio[i] * gain;
-            LOGGER.info(value);
+            float value = audio[i]-lastValue;
+            lastValue = audio[i];
+            value *= gain;
             if (value >  32767.0f) {
                 value = 32767.0f;
             } else if (value < -32767.0f) {
                 value = -32767.0f;
             }
-//            value = (short) (value / 512);
-//            LOGGER.info(value);
-//            value *= 512;
             newAudio[i] = (short) value;
-            LOGGER.info(value);
         }
         return newAudio;
     }
