214b67796f03bc252086c4ccd53b91cb9394c42c
Sluym1k <Denis.kerstens@gmail.com>
2026-07-26 21:56:17 +0300

    Returned holding animation

--- common/src/main/java/com/sluym1k/items/custom/Megaphone.java
+++ common/src/main/java/com/sluym1k/items/custom/Megaphone.java
@@ -2,6 +2,7 @@ package com.sluym1k.items.custom;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -20,10 +21,11 @@ public class Megaphone extends Item {
         return InteractionResultHolder.consume(itemstack);
     }
 
-//    @Override
-//    public int getUseDuration(ItemStack stack) {
-//        return 72000;
-//    }
+
+    @Override
+    public int getUseDuration(ItemStack itemStack, LivingEntity livingEntity) {
+        return 72000;
+    }
 
     @Override
     public UseAnim getUseAnimation(ItemStack stack) {
