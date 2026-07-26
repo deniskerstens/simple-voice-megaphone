ec881afc1ada509ffe48dfb0110aa483ad7d1376
Sluym1k <Denis.kerstens@gmail.com>
2026-07-26 23:58:42 +0300

    Removed forge folder

--- forge/src/main/java/com/sluym1k/forge/SimpleMegaphoneVoicechatPluginForge.java
+++
@@ -1,8 +0,0 @@
-package com.sluym1k.forge;
-
-import com.sluym1k.SimpleMegaphoneVoicechatPlugin;
-import de.maxhenkel.voicechat.api.ForgeVoicechatPlugin;
-
-@ForgeVoicechatPlugin
-public class SimpleMegaphoneVoicechatPluginForge extends SimpleMegaphoneVoicechatPlugin {
-}
